c01050250ef23592bad0d1649102a089a1c1e205
Cory Johns <johnsca@gmail.com>
2018-08-01 13:03:41 -0400

    Add support for auto-fetch

--- reactive/docker_resource.py
+++ reactive/docker_resource.py
@@ -1,6 +1,18 @@
-from charms.reactive import when
+from charmhelpers.core import hookenv
+from charms.reactive import set_flag, when, when_not
 
 from charms import layer
+
+
+@when_not('layer.docker-resource.auto-fetched')
+def auto_fetch():
+    resources = hookenv.metadata().get('resources', {})
+    for name, resource in resources.items():
+        is_docker = resource.get('type') == 'docker'
+        is_auto_fetch = resource.get('auto-fetch', False)
+        if is_docker and is_auto_fetch:
+            layer.docker_resource.fetch(name)
+    set_flag('layer.docker-resource.auto-fetched')
 
 
 @when('layer.docker-resource.pending')
